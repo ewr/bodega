c1bd0d919f9dabde7a774e090205d94cd4404ca9
Eric Richardson <e@ericrichardson.com>
2015-04-10 22:40:44 -0400

    Switch Chef client packages

--- minimart/minimart.go
+++ minimart/minimart.go
@@ -6,21 +6,24 @@ import (
 	"encoding/json"
 	"fmt"
 	_ "github.com/ctdk/goiardi/cookbook"
-	"github.com/go-chef/chef"
-	_ "github.com/marpaia/chef-golang"
+	_ "github.com/go-chef/chef"
+	"github.com/marpaia/chef-golang"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"sync"
 	"time"
+    "crypto/rsa"
+	"encoding/pem"
+	"crypto/x509"
+    "errors"
 )
 
 type ProxyCookbookVersion struct {
 	LocationPath string            `json:"location_path"`
 	LocationType string            `json:"location_type"`
 	Dependencies map[string]string `json:"dependencies"`
-	Cookbook     chef.Cookbook     `json:"-"`
+	CookbookVersion     *chef.CookbookVersion     `json:"-"`
 }
 
 type UniverseHandler struct {
@@ -32,18 +35,24 @@ type Minimart struct {
 
 	cookbooks  map[string]map[string]*ProxyCookbookVersion
 	chefServer string
-	client     *chef.Client
+	client     *chef.Chef
 	ticker     *time.Ticker
 	tickerDone chan struct{}
 
 	pollMutex sync.RWMutex
 }
 
+// Config is used to pass configuration options into the NewMinimart() constructor.
 type Config struct {
+    // URI to the Chef Server, with protocol and organization path (if required)
     ChefServer string
+    // Path to the file containing the client PEM
     ChefPEM string
+    // Name of the Chef client
     ChefClient string
+    // Base URL to use in the universe endpoint
     BaseURL string
+    // Disable SSL validation for the Chef server
     SkipSSL bool
 }
 
@@ -51,22 +60,22 @@ func NewMinimart(config *Config) *Minimart {
 	// read PEM file
 	key, err := ioutil.ReadFile(config.ChefPEM)
 	if err != nil {
-		fmt.Println("Couldn't read key.pem:", err)
-		os.Exit(1)
+		log.Fatal("Couldn't read key.pem: %v", err)
 	}
 
-	client, err := chef.NewClient(&chef.Config{
-		Name:    config.ChefClient,
-		Key:     string(key),
-		BaseURL: config.ChefServer,
-		SkipSSL: true,
-	})
+    rsaKey, err := keyFromString(key)
 
-	if err != nil {
-		log.Fatal("Error connecting to Chef server: %v", err)
-	}
+    if err != nil {
+        log.Fatal("Failed to parse key: %v", err)
+    }
 
-	//client.SSLNoVerify = true
+    client := &chef.Chef{
+        Url:    config.ChefServer,
+        Key:    rsaKey,
+        UserId: config.ChefClient,
+        SSLNoVerify: config.SkipSSL,
+        Version: "11.6.0",
+    }
 
 	return &Minimart{
 		baseURL:    config.BaseURL,
@@ -75,6 +84,19 @@ func NewMinimart(config *Config) *Minimart {
 		ticker:     nil,
 		tickerDone: make(chan struct{}),
 	}
+}
+
+// keyFromString parses an RSA private key from a string
+func keyFromString(key []byte) (*rsa.PrivateKey, error) {
+	block, _ := pem.Decode(key)
+	if block == nil {
+		return nil, fmt.Errorf("block size invalid for '%s'", string(key))
+	}
+	rsaKey, err := x509.ParsePKCS1PrivateKey(block.Bytes)
+	if err != nil {
+		return nil, err
+	}
+	return rsaKey, nil
 }
 
 func (c *Minimart) NewUniverseHandler() *UniverseHandler {
@@ -98,7 +120,9 @@ func (c *Minimart) scrapeCookbooks() {
 	c.pollMutex.Lock()
 	defer c.pollMutex.Unlock()
 
-	all_versions, err := c.client.Cookbooks.ListAvailableVersions("all")
+    // We can't use chef.GetCookbooks() because we need to get all cookbook
+    // versions. For now we're duplicating the logic below...
+    all_versions, err := c.getAllCookbooks()
 
 	if err != nil {
 		log.Printf("Error fetching cookbooks: %v", err)
@@ -112,18 +136,23 @@ func (c *Minimart) scrapeCookbooks() {
 
 		versions := make(map[string]*ProxyCookbookVersion)
 		for _, v := range cvlist.Versions {
-			cv, err := c.client.Cookbooks.GetVersion(name, v.Version)
+			cv, ok, err := c.client.GetCookbookVersion(name, v.Version)
 
 			if err != nil {
 				log.Printf("error fetching cookbook version: %v", err)
 				break
 			}
 
+            if !ok {
+                log.Printf("Couldn't find cookbook version: %s/%s", name, v.Version)
+                break
+            }
+
 			versions[v.Version] = &ProxyCookbookVersion{
 				LocationPath: fmt.Sprintf("%s/cookbooks/%s/%s/download", c.baseURL, name, v.Version),
 				LocationType: "supermarket",
-				Dependencies: cv.Metadata.Depends,
-				Cookbook:     cv,
+				Dependencies: cv.Metadata.Dependencies,
+				CookbookVersion:     cv,
 			}
 
 			log.Printf("got %v/%v", name, v.Version)
@@ -134,6 +163,28 @@ func (c *Minimart) scrapeCookbooks() {
 
 	c.cookbooks = cookbooks
 	log.Printf("Cookbook cache complete.")
+}
+
+func (c *Minimart) getAllCookbooks() (map[string]*chef.Cookbook, error) {
+	resp, err := c.client.Get("cookbooks?num_versions=all")
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, errors.New(resp.Status)
+	}
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	resp.Body.Close()
+
+	cookbooks := map[string]*chef.Cookbook{}
+	json.Unmarshal(body, &cookbooks)
+
+	return cookbooks, nil
 }
 
 func (c *Minimart) PollForCookbooks(interval time.Duration) {
@@ -172,13 +223,18 @@ func (c *Minimart) CreateCookbookVersionTarball(name, version string) (*bytes.Bu
 
 	buf := new(bytes.Buffer)
 
-	cookbook := c.cookbooks[name][version].Cookbook
+	cookbook := c.cookbooks[name][version].CookbookVersion
 
 	// basically we have to iterate each file in the cookbook, adding them
 	// into a tar as we get them. We then gzip that and send it along.
-	for _, file := range cookbook.Files {
-		log.Printf("Cookbook file is %s", file.Name)
-	}
+    sections := [][]struct {chef.CookbookItem}{cookbook.Files, cookbook.Templates, cookbook.Recipes, cookbook.Attributes, cookbook.Definitions, cookbook.Libraries, cookbook.Providers, cookbook.Resources, cookbook.RootFiles}
+
+    for _, section := range sections {
+        log.Printf("Section is %v", section)
+    	for _, file := range section {
+    		log.Printf("Cookbook file is %s - %s", file.Name, file.Path)
+    	}
+    }
 
 	return buf, nil
 }
